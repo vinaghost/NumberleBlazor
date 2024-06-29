1f9bac4a250765e511e8e1de33707dbaf410639d
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-06-29 14:32:08 +0700

    api for solution

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -1,3 +1,5 @@
+using BlazorApp;
+using BlazorApp.Client.Model.Enums;
 using BlazorApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,5 +26,76 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorApp.Client._Imports).Assembly);
+
+app.MapGet("/api/solution", () => SolutionEncryption.Encrypt((int)(DateTime.Now.Ticks % 100000)));
+app.MapGet("/api/check", (string key, string guess) =>
+{
+    var solution = SolutionEncryption.Decrypt(key);
+    var splitSolution = solution.ToCharArray();
+
+    var splitGuess = guess.ToCharArray();
+
+    var solutionCharsTaken = Enumerable.Repeat(false, splitSolution.Length).ToArray();
+
+    var statuses = new BoardCellState[splitSolution.Length];
+
+    /*
+    Correct Cases
+    */
+
+    foreach (var item in splitGuess.Select((letter, i) => new { i, letter }))
+    {
+        if (item.letter == splitSolution[item.i])
+        {
+            statuses[item.i] = BoardCellState.Correct;
+            solutionCharsTaken[item.i] = true;
+        }
+    }
+
+    if (Array.TrueForAll(statuses, x => x == BoardCellState.Correct))
+    {
+        return statuses;
+    }
+
+    /*
+    Wrong Cases
+    */
+
+    foreach (var item in splitGuess.Select((letter, i) => new { i, letter }))
+    {
+        if (statuses[item.i] == BoardCellState.Correct) continue;
+
+        if (!Array.Exists(splitSolution, elem => elem == item.letter))
+        {
+            statuses[item.i] = BoardCellState.Wrong;
+            continue;
+        }
+
+        /*
+        IncorrectPosition Cases
+        */
+
+        int indexOfPresentChar = Array.FindIndex(splitSolution, x => x == item.letter);
+
+        if (indexOfPresentChar > -1)
+        {
+            if (!solutionCharsTaken[indexOfPresentChar])
+            {
+                statuses[item.i] = BoardCellState.IncorrectPosition;
+                solutionCharsTaken[indexOfPresentChar] = true;
+            }
+            else
+            {
+                statuses[item.i] = BoardCellState.Wrong;
+            }
+        }
+        else
+        {
+            statuses[item.i] = BoardCellState.Wrong;
+        }
+    }
+
+    return statuses;
+});
 
 await app.RunAsync();
---
+++ BlazorApp/BlazorApp/SolutionEncryption.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorApp
+{
+    public static class SolutionEncryption
+    {
+        public static string Encrypt(int k)
+        {
+            var str = Convert.ToBase64String(BitConverter.GetBytes(k));
+            var reverseStr = Reverse(str);
+            var swapStr = reverseStr[^1] + reverseStr[1..^1] + reverseStr[0];
+            return swapStr;
+        }
+
+        public static string Decrypt(string encoded)
+        {
+            var swapStr = encoded[^1] + encoded[1..^1] + encoded[0];
+            var reverseStr = Reverse(swapStr);
+            var buffer = new Span<byte>(new byte[reverseStr.Length]);
+            if (!Convert.TryFromBase64String(reverseStr, buffer, out _))
+            {
+                return "00000";
+            }
+            return BitConverter.ToInt32(buffer.ToArray(), 0).ToString();
+        }
+
+        private static string Reverse(string input)
+        {
+            return string.Create(input.Length, input, (chars, state) =>
+            {
+                state.AsSpan().CopyTo(chars);
+                chars.Reverse();
+            });
+        }
+    }
+}
